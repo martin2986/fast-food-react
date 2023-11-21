21aed4734b2da98ae1146e37eb058c28f288a472
martins2986 <m.devit@pm.me>
2023-11-21 09:15:06 +0100

    app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import "./App.scss";
+import { Container } from "react-bootstrap";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container>
+      <h1>App</h1>
+    </Container>
+  );
 };
 
 export default App;
